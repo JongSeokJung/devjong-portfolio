fbac402ba9fcd23c27245d4064624fe30d46292a
Jongseok Jung <jong03720@gmail.com>
2023-06-11 16:23:31 -0700

    Set up basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
-function App() {
+type Props = {};
+
+const App = (props: Props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Nav />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/about/About.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./about.css";
+
+type Props = {};
+
+const About = (props: Props) => {
+  return <div>About</div>;
+};
+
+export default About;
